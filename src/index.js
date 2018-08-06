eb9e28f46eacdef608d6f117fb9186e29c93e36d
Shamnad Shaji <shamnad.shaji@gofore.com>
2018-08-07 00:16:13 +0300

    Added a person Component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import { React } from 'react';
-import { ReactDOM } from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './index.css'
-import App from App;
-import registreServiceWorker from './registreServiceWorker';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registreServiceWorker();
+registerServiceWorker();
